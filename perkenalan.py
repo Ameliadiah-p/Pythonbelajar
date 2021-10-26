7308e28941864ec96aa88502cdbebeee0964e839
Ameliadiah-p <ameliadiah.ad@gmail.com>
2021-10-26 23:08:18 +0700

    perkenalan

---
+++ perkenalan.py
@@ -0,0 +1,14 @@
+fullname = input("my name is : ")
+nickname = input("nickname is : ")
+nim = input("nim : ")
+born = input("born in : ")
+age = input("age : ")
+address = input("address : ")
+phonenumber = input("phonenumber : ")
+
+print("===============")
+print("assalamualaikum")
+
+print("Let me introduce my self.")
+print("My name is %s, but you can call me %s. My nim is %s. I was born in %s, and i am %s years old. I am very glad if you want to invite my house in %s. So, don't forget to call me before with the phonenumber %s" % (fullname, nickname, nim, born, age, address, phonenumber))
+print("thank you")
